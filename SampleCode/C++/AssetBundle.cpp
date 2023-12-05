869308133d1c3bf11f63e19834a056688d0331b3
Hiroshi Minakata <23pg0037@osaka.amg.ac.jp>
2023-12-05 10:28:44 +0900

    update sample cpp

--- SampleCode/C++/AssetBundle.cpp
+++ SampleCode/C++/AssetBundle.cpp
@@ -2,10 +2,87 @@
 
 #include "AssetBundle.h"
 
-AssetBundle::AssetBundle(const std::string& fileName) : std::ifstream(fileName, std::ios::binary)
+#pragma region AssetReader
+AssetReader::AssetReader(const std::string& fileName, std::streampos offset)
+	: std::ifstream(fileName, std::ios::binary), fileName(fileName)
 {
 	if (!is_open())
 		throw std::runtime_error("Can't open file: " + fileName);
+	seekg(offset);
+}
+
+bool AssetReader::IsUse()
+{
+	bool isUse = false;
+	read(reinterpret_cast<char*>(&isUse), sizeof(bool));
+	if (fail())
+		throw std::runtime_error("Read error: Failed to read IsUse");
+	return isUse;
+}
+
+void AssetReader::ReadString(std::string* str, bool skip)
+{
+	int strLength = Get7BitInt32();
+	if (skip)
+	{
+		seekg(strLength, std::ios::cur);
+		return;
+	}
+	str->resize(strLength);
+	read(&(*str)[0], strLength);
+	if (fail())
+		throw std::runtime_error("Read error: Failed to read string");
+}
+
+void AssetReader::ReadBytes(std::vector<char>* bytes, bool skip)
+{
+	int64_t size = GetBytesSize();
+	if (skip)
+	{
+		seekg(size, std::ios::cur);
+		return;
+	}
+	bytes->resize(size);
+	read(bytes->data(), size);
+	if (fail())
+		throw std::runtime_error("Read error: Failed to read bytes");
+}
+
+int AssetReader::Get7BitInt32()
+{
+	int num = 0;
+	int shift = 0;
+	char byte;
+	do {
+		if (shift == 5 * 7)
+			throw std::runtime_error("Read error: Bad format 7BitInt32");
+		read(&byte, 1);
+		if (fail())
+			throw std::runtime_error("Read error: Failed to read 7-bit int");
+		num |= (byte & 0x7F) << shift;
+		shift += 7;
+	} while (byte & 0x80);
+	return num;
+}
+
+int AssetReader::GetArrayLength()
+{
+	int length;
+	read(reinterpret_cast<char*>(&length), sizeof(int));
+	return length;
+}
+
+int64_t AssetReader::GetBytesSize()
+{
+	int64_t size;
+	read(reinterpret_cast<char*>(&size), sizeof(int64_t));
+	return size;
+}
+#pragma endregion
+
+#pragma region AssetBundle
+AssetBundle::AssetBundle(const std::string& fileName) : AssetReader(fileName)
+{
 	offsets.clear();
 	int assetNum = LoadHeader();
 	LoadMetaData(assetNum);
@@ -38,65 +115,6 @@ void AssetBundle::Unload(bool all)
 	offsets.clear();
 }
 
-bool AssetBundle::IsUse()
-{
-	bool isUse = false;
-	read(reinterpret_cast<char*>(&isUse), sizeof(bool));
-	if (fail())
-		throw std::runtime_error("Read error: Failed to read IsUse");
-	if (!isUse) return false;
-	else return true;
-}
-
-void AssetBundle::ReadString(std::string* str)
-{
-	int strLength = Get7BitInt32();
-	str->resize(strLength);
-	read(reinterpret_cast<char*>(str->data()), strLength);
-	if (fail())
-		throw std::runtime_error("Read error: Failed to read string");
-}
-
-void AssetBundle::ReadBytes(std::vector<char>* bytes)
-{
-	int64_t size = GetBytesSize();
-	bytes->resize(size);
-	read(bytes->data(), size);
-	if (fail())
-		throw std::runtime_error("Read error: Failed to read bytes");
-}
-
-int AssetBundle::Get7BitInt32()
-{
-	int num = 0;
-	int shift = 0;
-	char byte;
-	do {
-		if (shift == 5 * 7)
-			throw std::runtime_error("Read error: Bad format 7BitInt32");
-		read(&byte, 1);
-		if (fail())
-			throw std::runtime_error("Read error: Failed to read 7-bit int");
-		num |= (byte & 0x7F) << shift;
-		shift += 7;
-	} while (byte & 0x80);
-	return num;
-}
-
-int AssetBundle::GetArrayLength()
-{
-	int length;
-	read(reinterpret_cast<char*>(&length), sizeof(int));
-	return length;
-}
-
-int64_t AssetBundle::GetBytesSize()
-{
-	int64_t size;
-	read(reinterpret_cast<char*>(&size), sizeof(int64_t));
-	return size;
-}
-
 int AssetBundle::LoadHeader()
 {
 	int saveMode;
@@ -109,15 +127,15 @@ int AssetBundle::LoadHeader()
 	int64_t totalAssetSize;
 	int footerSize;
 
-	read(reinterpret_cast<char*>(&saveMode),	   sizeof(int));
-	read(reinterpret_cast<char*>(fileID.data()),   Get7BitInt32());
-	read(reinterpret_cast<char*>(&toolVesion),	   sizeof(int));
-	read(reinterpret_cast<char*>(&version),		   sizeof(int));
-	read(reinterpret_cast<char*>(&assetNum),	   sizeof(int));
-	read(reinterpret_cast<char*>(&headerSize),	   sizeof(int));
-	read(reinterpret_cast<char*>(&metaDataSize),   sizeof(int));
+	read(reinterpret_cast<char*>(&saveMode), sizeof(int));
+	read(&fileID[0], Get7BitInt32());
+	read(reinterpret_cast<char*>(&toolVesion), sizeof(int));
+	read(reinterpret_cast<char*>(&version), sizeof(int));
+	read(reinterpret_cast<char*>(&assetNum), sizeof(int));
+	read(reinterpret_cast<char*>(&headerSize), sizeof(int));
+	read(reinterpret_cast<char*>(&metaDataSize), sizeof(int));
 	read(reinterpret_cast<char*>(&totalAssetSize), sizeof(int64_t));
-	read(reinterpret_cast<char*>(&footerSize),	   sizeof(int));
+	read(reinterpret_cast<char*>(&footerSize), sizeof(int));
 
 	return assetNum;
 }
@@ -133,10 +151,11 @@ void AssetBundle::LoadMetaData(int assetNum)
 
 		int strLength = Get7BitInt32();
 		assetName.resize(strLength);
-		read(reinterpret_cast<char*>(assetName.data()), strLength);
-		read(reinterpret_cast<char*>(&fieldNum),		sizeof(int));
-		read(reinterpret_cast<char*>(&offset),			sizeof(int64_t));
-		read(reinterpret_cast<char*>(&size),			sizeof(int64_t));
+		read(&assetName[0], strLength);
+		read(reinterpret_cast<char*>(&fieldNum), sizeof(int));
+		read(reinterpret_cast<char*>(&offset), sizeof(int64_t));
+		read(reinterpret_cast<char*>(&size), sizeof(int64_t));
 		offsets.emplace(assetName, offset);
 	}
 }
+#pragma endregion
--- SampleCode/C++/AssetBundle.h
+++ SampleCode/C++/AssetBundle.h
@@ -1,18 +1,18 @@
 /*
 * MIT License
-* 
+*
 * Copyright(c) 2023 HiroshiMinakata
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files(the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions :
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
@@ -69,7 +69,6 @@
 */
 
 // AssetBundle.h
-//export module AssetBundle;
 
 #pragma once
 #include <fstream>
@@ -77,34 +76,26 @@
 #include <unordered_map>
 #include <vector>
 
-class AssetBundle : public std::ifstream
+#pragma region AssetReader
+class AssetReader : public std::ifstream
 {
 public:
-	AssetBundle(const std::string& fileName);
-	~AssetBundle();
+	AssetReader(const std::string& fileName, std::streampos offset = 0);
 
 	template <typename T>
-	T* Load(std::string name)
+	void Read(T* data, bool getIsUse = true, bool skip = false)
 	{
-		T* asset = new T;
-		asset->Decode(this, offsets[name]);
-		anyPtrMaps.emplace(name, asset);
-		return asset;
-	}
-	void FileClose();
+		if (getIsUse)
+			skip = !IsUse();
 
-	void Unload(std::string name);
-	void Unload(bool all);
+		int a = 0;
 
-	template <typename T>
-	void Read(T* data, bool ignoreIsUse = false)
-	{
-		if (!ignoreIsUse && !IsUse()) return;
-
+		// Get string
 		if constexpr (std::is_same<T, std::string>::value)
-			ReadString(data); // Get string
+			ReadString(data, skip);
+		// Get Bytes
 		else if constexpr (std::is_same<T, std::vector<char>>::value)
-			ReadBytes(data); // Get Bytes
+			ReadBytes(data, skip);
 		else if constexpr (is_vector_not_char<T>::value || is_array<T>::value)
 		{
 			// Get length
@@ -112,22 +103,23 @@ public:
 
 			// Resize if T is a vector
 			if constexpr (is_vector_not_char<T>::value)
-				data->resize(length);
+				if (data->size() != length)
+					data->resize(length);
 
 			// Get vector datas or array datas
 			for (int i = 0; i < length; i++)
 			{
 				if constexpr (std::is_pointer<typename std::remove_reference<decltype((*data)[i])>::type>::value)
-					Read((*data)[i], true); // Is ptr
+					Read((*data)[i], false, skip); // Is ptr
 				else
-					Read(&(*data)[i], true); // Not ptr
+					Read(&(*data)[i], false, skip); // Not ptr
 			}
 		}
-		else read(reinterpret_cast<char*>(data), sizeof(T));
+		else
+			(skip) ? seekg(sizeof(T), std::ios::cur) : read(reinterpret_cast<char*>(data), sizeof(T));
 		if (fail())
 			throw std::runtime_error("Read error: Failed to read");
 	}
-
 private:
 	// for vector and not char vector
 	template <typename T>
@@ -140,16 +132,48 @@ private:
 	struct is_array : std::false_type {};
 	template <typename T, std::size_t N>
 	struct is_array<T[N]> : std::true_type {};
-	
+
+protected:
 	bool IsUse();
 
-	void ReadString(std::string*);
-	void ReadBytes(std::vector<char>*);
+	void ReadString(std::string* str, bool skip);
+	void ReadBytes(std::vector<char>* bytes, bool skip);
 
 	int Get7BitInt32();
 	int GetArrayLength();
 	int64_t GetBytesSize();
 
+protected:
+	const std::string fileName;
+};
+#pragma endregion
+
+#pragma region AssetBundle
+class AssetBundle : public AssetReader
+{
+public:
+	AssetBundle(const std::string& fileName);
+	~AssetBundle();
+
+	template <typename T>
+	T* Load(std::string name)
+	{
+		T* asset = new T;
+
+		// Check have Decode
+		if constexpr (std::is_invocable<decltype(&T::Decode), T*, const std::string&, const int64_t&>::value)
+			asset->Decode(fileName, offsets[name]);
+		else
+			static_assert(false, "Decode method is missing in the class.");
+		anyPtrMaps.emplace(name, asset);
+		return asset;
+	}
+	void FileClose();
+
+	void Unload(std::string name);
+	void Unload(bool all);
+
+private:
 	int LoadHeader();
 	void LoadMetaData(int assetNum);
 
@@ -157,3 +181,4 @@ private:
 	std::unordered_map<std::string, int64_t> offsets;
 	std::unordered_map<std::string, void*> anyPtrMaps;
 };
+#pragma endregion
